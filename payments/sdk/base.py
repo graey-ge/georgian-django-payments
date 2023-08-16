8cf4e480b22927ca2872bc6e3472a5cba76349f9
Lasha Gogorishvili <lashagogorishvili1999@gmail.com>
2023-08-16 23:00:05 +0400

    Added Base Bank SDK

---
+++ payments/sdk/base.py
@@ -0,0 +1,60 @@
+from typing import TYPE_CHECKING, Dict, Tuple
+
+from django.templatetags.static import static
+
+if TYPE_CHECKING:
+    from payments.models import PaymentTransaction
+
+
+class AbstractBankSDK:
+    _NAME = None
+    unique_by_key = None
+    pan_key = None
+    image_path = None
+    _PAY_URL = '%s'
+
+    def __init__(self, transaction: 'PaymentTransaction', **kwargs):
+        self.transaction: PaymentTransaction = transaction
+
+    def __str__(self):
+        assert self._NAME is not None, "Field: _NAME Must Be Implemented"
+        return self._NAME
+
+    @classmethod
+    def display(cls):
+        return cls._NAME
+
+    @staticmethod
+    def get_failed_text_status():
+        return 'Contact To Bank'
+
+    def start_payment(self) -> Dict:
+        """
+        :return: {
+        'status': bool,
+        'redirect_url': str,
+        'trx_id': str,
+        'payment_hash': str
+        }
+        """
+        raise NotImplementedError
+
+    def check_transaction_status(self) -> Tuple[dict, int]:
+        raise NotImplementedError
+
+    def refund(self, amount) -> Tuple[bool, Dict]:
+        raise NotImplementedError
+
+    def cancel(self, amount) -> Tuple[bool, Dict]:
+        raise NotImplementedError
+
+    def cash_back(self, amount) -> Tuple[bool, Dict]:
+        pass
+
+    @classmethod
+    def image_url(cls):
+        return static(cls.image_path)
+
+    @classmethod
+    def pay_url(cls, trx):
+        return cls._PAY_URL % trx
